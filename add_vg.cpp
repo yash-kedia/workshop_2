5e7110d9e88e3c30b3d46204ff235c0e54b67697
Vaibhav Gupta <vaibhavgupta40@gmail.com>
2017-11-04 22:49:11 +0530

    hii (#22)
    * hii
    
    * learning

---
+++ add_vg.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int a=2,b=5;
+cout<<a+b;
+return 0;
+
+ }
---
+++ trial.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+ int a=2,b=5;
+printf("%d",a+b);
+
+ }
